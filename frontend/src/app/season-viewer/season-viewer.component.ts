aba1ad050f8101b5147ac55bc08bee729886a88f
Thomas Dunajski <thomas.dunajski@gfnork.de>
2020-06-30 18:37:04 +0200

    fixed off by 1 error

--- frontend/src/app/season-viewer/season-viewer.component.ts
+++ frontend/src/app/season-viewer/season-viewer.component.ts
@@ -15,5 +15,5 @@ export class SeasonViewerComponent implements OnInit {
 
   months = ["Jan", "Feb", "Mär", "Apr", "Mai", "Jun", "Jul", "Aug", "Sep", "Okt", "Nov", "Dez" ];
   inSeason = (number) => this.season.indexOf(number) === -1 ? false : true;
-  cuurentlyInSeaseon = () => this.inSeason(new Date().getMonth()) ? "grade in Saison" : "grade nicht in Saison";
+  cuurentlyInSeaseon = () => this.inSeason(new Date().getMonth() +1) ? "grade in Saison" : "grade nicht in Saison";
 }
