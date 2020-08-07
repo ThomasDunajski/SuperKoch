fc7400c960a4e40af3a4f78e24b072d8bde01582
Thomas Dunajski <thomas.dunajski@gfnork.de>
2020-08-07 16:14:43 +0200

    doubled thummbnail size colored view mode selection buttons in search

--- backend/thumbnail.js
+++ backend/thumbnail.js
@@ -2,7 +2,7 @@ var sharp = require('sharp');
  
 exports.create = function(path, newPath){ 
   sharp(path)
-    .resize(200, 200)
+    .resize(400, 400)
     .toFile(newPath, function(err) {
       if(err) console.log(err);
     });
