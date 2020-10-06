ce89011a8ffcc145fe3744913fbbd539dfac8e88
Thomas Dunajski <thomas.dunajski@gfnork.de>
2020-10-06 19:28:47 +0200

    adding/removing sections implemented

--- frontend/src/app/edit-collection/edit-collection.component.ts
+++ frontend/src/app/edit-collection/edit-collection.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-edit-collection',
@@ -8,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class EditCollectionComponent implements OnInit {
 
   constructor() { }
+
   collection = {name:"", text:"", sections:[{name:"", recipes:[1,2,3]}]}
   recipeMock = {name:"mock", imageUri:"api/images/1597757341445.xdbZA.jpg"}
+  animal;
+  name;
   ngOnInit(): void {
   }
-
-}
+  addRecipe = (i)=> {
+    this.collection.sections[i].recipes.push(1);
+  }
+  removeRecipe = (i,j)=> {
+    this.collection.sections[i].recipes.splice(j,1);
+  }
+  addSection = ()=> {
+    this.collection.sections.push({name:"", recipes:[]})
+  }
+  removeSection = (i)=> {
+    this.collection.sections.splice(i,1);
+  }
+ }
