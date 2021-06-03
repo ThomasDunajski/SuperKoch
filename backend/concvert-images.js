fe74268c7e7bf943adf0cdf40033eac0e212cf49
Thomas Dunajski <thomas.dunajski@gfnork.de>
2021-06-03 15:55:55 +0200

    changed image compression

---
+++ backend/concvert-images.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+var sharp = require('sharp');
+
+const dir = fs.opendirSync(process.argv[2])
+let dirent
+while ((dirent = dir.readSync()) !== null) {
+        console.log(process.argv[2] +  dirent.name.replace('.jpg', '.webp'))
+    sharp(process.argv[2] +'\\' + dirent.name)
+    .webp({quality:80})
+    .toFile((process.argv[2] +'\\' + dirent.name).replace('.jpg', '.webp'))
+    .catch((err) =>{
+      console.log(err.message);
+    }); 
+
+}
+dir.closeSync()
